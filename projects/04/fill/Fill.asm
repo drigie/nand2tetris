fe487583b648fc5c03002ba808541a59eb145667
David Rigie <drigie@reflexion.com>
2020-07-03 15:35:03 -0700

    complete chapter 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    // Check the keyboard
+    @KBD
+    D = M
+    
+    
+    @BLACK
+    D ; JGT
+    // ELSE
+        @screenval 
+        M=0 // default to 0
+        @ENDIF
+        0 ; JMP
+    (BLACK)
+        @screenval 
+        M=-1
+        
+    (ENDIF)
+    @8191 // initialize loop counter at max offset
+    D=A
+    @i
+    M=D
+    (SCREENLOOP)
+        @i
+        D=M
+        @LOOP
+        D ; JLT // termination condition
+
+        @SCREEN
+        D=D+A // address points to correct pixel group
+        @screenpointer
+        M=D
+        @screenval
+        D=M
+        @screenpointer
+        A=M
+        M=D      
+
+        @i
+        M=M-1 // decrement i
+        
+        @SCREENLOOP
+        0 ; JMP
+            
+@LOOP
+0 ; JMP // infinite loop
